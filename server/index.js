089009d3f2b8217c321786e42a3af66ef862b78d
cygyi <chygyi@163.com>
2025-04-27 20:33:20 +0800

    feat: add Cloudflare CDN configuration

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization']
 }))
 
+// 添加响应头中间件
+app.use((req, res, next) => {
+  res.setHeader('X-Content-Type-Options', 'nosniff')
+  res.setHeader('X-Frame-Options', 'DENY')
+  res.setHeader('X-XSS-Protection', '1; mode=block')
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains')
+  next()
+})
+
 app.use(express.json())
 
 // 添加请求日志中间件
